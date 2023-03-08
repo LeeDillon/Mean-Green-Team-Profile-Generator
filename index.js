1a70dfc3555d152f3903358ceb13e433db78f30c
Lee-Dillon <lee.dillon1@outlook.com>
2023-03-08 19:15:40 +0000

    eod 08/03

--- index.js
+++ index.js
@@ -12,4 +12,36 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const employeeArray = [];
 
+function initQuestions() {
+    inquirer.prompt(
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Managers name?',
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: 'What is the Managers Employee ID?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the Managers Email address?',
+        },
+        {
+            type: 'input',
+            name: 'officeNum',
+            message: 'What is the Managers Office Number?',
+        }).then((answers) =>  new Manager(answers.name, answers.employeeID, answers.email, answers.officeNum),
+            employeeArray.push(answers),
+        console.log(employeeArray))
+}
+
+// function createEngineer() {
+
+// }
+
+initQuestions();
