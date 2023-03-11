bc8ed9d8d8d86a9faf59198b5a7750856a94ced1
Lee-Dillon <lee.dillon1@outlook.com>
2023-03-11 16:50:29 +0000

    thought i was finished but inquirer exits before I can answer a question

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const render = require("./src/page-template.js");
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 const employeeArray = [];
 
-function initQuestions() {
+function createManager() {
     inquirer.prompt(
-        {
+        [{
             type: 'input',
             name: 'name',
             message: 'What is the Managers name?',
@@ -35,13 +35,88 @@ function initQuestions() {
             type: 'input',
             name: 'officeNum',
             message: 'What is the Managers Office Number?',
-        }).then((answers) =>  new Manager(answers.name, answers.employeeID, answers.email, answers.officeNum),
-            employeeArray.push(answers),
-        console.log(employeeArray))
+        }]).then((answers) => {
+            const manager = new Manager(answers.name, answers.employeeID, answers.email, answers.officeNum);
+            employeeArray.push(manager);
+            console.log(manager);
+        }
+        )
 }
 
-// function createEngineer() {
+function createEmployee() {
+    inquirer.prompt(
+        [{
+            type: 'list',
+            name: 'job',
+            message: 'What is the employees job?"=',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the employees name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the employees ID?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the employees email?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the engineers github username?',
+            when: (input) => input.job === 'Engineer'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the interns school?',
+            when: (input) => input.job === 'Intern'
+        },
+        {
+            type: 'confirm',
+            name: 'addExtraEmployee',
+            message: 'Would you like to add another Employee?',
+            default: false
+        }])
+        .then((answers) => {
+            if (job === 'Engineer') {
+                const engineer = new Engineer(answers.name, answers.employeeID, answers.email, answers.github);
+                employeeArray.push(engineer);
+                console.log(engineer);
+            }
+            else if (job === 'Intern') {
+                const intern = new Intern(answers.name, answers.employeeID, answers.email, answers.school);
+                employeeArray.push(intern);
+                console.log(intern);
+            }
+            if (addExtraEmployee) {
+                return createEmployee(employeeArray);
+            } else {
+                return employeeArray;
+            }
+        }
+        )
+}
 
-// }
+
+function initQuestions() {
+    createManager()
+        .then(createEmployee)
+        .then(employeeArray => {
+            return render(employeeArray);
+        })
+        .then(HTMLblock => {
+            return fs.writeFileSync(outputPath, HTMLblock, "UTF-8");
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
 
 initQuestions();
